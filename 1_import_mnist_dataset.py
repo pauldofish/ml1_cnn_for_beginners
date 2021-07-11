4c7b3f2eefe5418eae8a08e0a79fad56e362142d
Paul Do <pauldofish@gmail.com>
2021-07-11 16:17:42 -0700

    Initial commit. Sanity check to see that python environment & packages are working

---
+++ 1_import_mnist_dataset.py
@@ -0,0 +1,13 @@
+# Sanity check to see that the pip packages are working properly.
+
+import numpy
+import mnist
+from tensorflow import keras
+
+# Assign mnist dataset to local arrays
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+
+# Print shape of arrays
+print(train_images.shape)
+print(train_labels.shape)
